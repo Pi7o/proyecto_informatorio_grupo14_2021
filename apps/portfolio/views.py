0a39c0229105bb2d031a406938a92d3b1eff5355
Pi7o <95898808+Pi7o@users.noreply.github.com>
2021-12-11 16:30:12 -0300

    Add files via upload

---
+++ apps/portfolio/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Post, PostView, Like, Comment
+from .forms import PostForm, CommentForm
+
+# modelos
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def post(self, *args, **kwargs):
+        form = CommentForm(self.request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            comment = form.instance
+            comment.user = self.request.user
+            comment.post = post
+            comment.save()
+            return redirect("detail", slug=post.slug)
+        return redirect("detail", slug=self.get_object().slug)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'form': CommentForm()})
+        return context
+
+    def get_object(self, **kwargs):
+        object = super().get_object(**kwargs)
+        if self.request.user.is_authenticated:
+            PostView.objects.get_or_create(user=self.request.user, post=object)
+
+        return object
+
+
+class PostCreateView(CreateView):
+    form_class = PostForm
+    model = Post
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'Create'
+        })
+        return context
+
+
+class PostUpdateView(UpdateView):
+    form_class = PostForm
+    model = Post
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'Update'
+        })
+        return context
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+
+def like(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    like_qs = Like.objects.filter(user=request.user, post=post)
+    if like_qs.exists():
+        like_qs[0].delete()
+        return redirect('detail', slug=slug)
+    Like.objects.create(user=request.user, post=post)
+    return redirect('detail', slug=slug)
+
+# Create your views here.
+# def index(request):
+#    hoy = datetime.datetime.now()
+#    html='<html><body><p> Hola! <br/> La fecha de hoy es</p> %s </body></html>' %hoy
+#    return HttpResponse(html)
