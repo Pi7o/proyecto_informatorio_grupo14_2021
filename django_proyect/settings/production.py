0f6936496d12a8bcd224114babbe00b670b76792
Pi7o <palacioivanisp@gmail.com>
2021-12-20 20:36:03 -0300

    postgresready

--- django_proyect/settings/production.py
+++ django_proyect/settings/production.py
@@ -1,5 +1,30 @@
 from .base import *
+import os
+import dj_database_url
+import django_heroku
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proyectoinf-grupo14',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=00)
+DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_URL = 'https://proyectoinf-grupo14.herokuapp.com/static/'
+MEDIA_URL = 'https://proyectoinf-grupo14.herokuapp.com/media/'
