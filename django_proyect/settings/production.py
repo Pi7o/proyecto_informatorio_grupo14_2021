7973501c9b01c761a5f96d54b10e3f8b9e09b11c
Pi7o <palacioivanisp@gmail.com>
2021-12-21 17:34:15 -0300

    msic

--- django_proyect/settings/production.py
+++ django_proyect/settings/production.py
@@ -5,5 +5,5 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = 'https://proyectoinf-grupo14.herokuapp.com/static/'
-MEDIA_URL = 'https://proyectoinf-grupo14.herokuapp.com/media/'
+#STATIC_URL = 'https://proyectoinf-grupo14.herokuapp.com/static/'
+#MEDIA_URL = 'https://proyectoinf-grupo14.herokuapp.com/media/'
