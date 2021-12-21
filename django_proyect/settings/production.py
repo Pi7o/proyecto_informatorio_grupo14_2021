dfab564ea5b1213b62237814ecf2db83f4d4b36d
Pi7o <palacioivanisp@gmail.com>
2021-12-21 10:02:24 -0300

    misc

--- django_proyect/settings/production.py
+++ django_proyect/settings/production.py
@@ -3,8 +3,7 @@ DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = 'https://proyectoinf-grupo14.herokuapp.com/static/'
-MEDIA_URL = 'https://proyectoinf-grupo14.herokuapp.com/media/'
+STATIC_URL = 'https://proyectoinf-grupo14.herokuapp.com/static'
+MEDIA_URL = 'https://proyectoinf-grupo14.herokuapp.com/media'
